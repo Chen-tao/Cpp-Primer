cf7090572342d430cd0649a176be1b6aebf422b5
Chen-tao <chentao2110334@vip.qq.com>
2015-10-31 18:43:20 +0800

    xcode init

---
+++ Cpp-Primer/Cpp-Primer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp-Primer
+//
+//  Created by 陈韬 on 31/10/2015.
+//  Copyright © 2015 chentao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return -1;
+}
